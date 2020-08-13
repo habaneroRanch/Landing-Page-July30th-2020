66f9b54aa170cec46e81fe4c7cc377b3a5e8b562
habeneroRanch <67810903+habeneroRanch@users.noreply.github.com>
2020-08-13 16:26:44 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -24,26 +24,55 @@ const sections = document.querySelectorAll('section');
 //anchor
 const a = document.querySelector('navbar__menu');
 
-
-
-
-
-
 /**
  * End Global Variables
  * Start Helper Functions
  * 
+
+/* I'm having trouble hiding the navbar, as learned from w3schools https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp */
+/* function hideDiv() {
+    navigation.style.visibility="hidden";
+}
+window.setTimeout(3000, hideDiv());
 */
+/*
+let prevScrollpos = window.pageYOffset; {
 
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar__menu").style.top = "0";
+  } else {
+    document.getElementById("navbar__menu").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+window.onscroll = function() {prevScrollpos()};
+ */  
+ /*
+I'm having trouble hiding the navbar, as learned from w3schools https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+//Get the button:
+const mybutton = document.getElementById("backToTop");
 
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.body.scrollTop > 20) {
+    mybutton.style.display = "inline";
+  } else {
+    mybutton.style.display = "none";
+  };
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+window.onscroll = function() {scrollFunction()};
+*/
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
-
-// build the nav
-
 // Navigation is built dynamically as an unordered list.
 function navBuild() {
     //looping over all sections
@@ -56,16 +85,13 @@ function navBuild() {
         //Use of template literals
         navUI += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
         
-         
     });
     // append all elements to the navigation
     navigation.innerHTML = navUI;
-
+    
 };
 
 navBuild();
-
-
 // Add class 'active' to section when near top of viewport
 
 // getting the largest value that's less or equal to the viewpoint number
@@ -85,7 +111,7 @@ function addActive(conditional, section) {
 
         section.classList.add('your-active-class');
         //Adding a change in background color to the active section.
-        section.style.cssText = "background-color: rgb(233, 183, 109)";
+        section.style.cssText = "background-color: rgb(0, 155, 155)";
         
         } else {
             section.classList.remove('your-active-class');
@@ -94,15 +120,12 @@ function addActive(conditional, section) {
         };
 
 };
-
-
 //Section Active State
-
 function sectionInvoked() {
     sections.forEach(section => {
         const elementPosition = position(section);
 
-        let inviewport = () => elementPosition < 250 && elementPosition >= -250;
+        let inviewport = () => elementPosition < 250 && elementPosition >= -1000;
 
         //revised "removeActive(section);""
         addActive(inviewport(), section);
@@ -110,53 +133,17 @@ function sectionInvoked() {
 };
 
 // Scroll to anchor ID using scrollTO event
-
-
-/*function scrolling() {
-    //a container for all the links
-    const links= document.querySelectorAll('.navbar__menu');
-    // per each link being worked on.
-    links.forEach(link => {
-        link.addEventListener('click', () =>{
-            for(i= 0; i<sections ; i++){
-                sections[i].addEventListener('click', href(link));
-                
-                
-                
-
-            };
-            
-        });
-    });
+function scrollTo(clicked){
+    clicked.scrollIntoView(sectionInvoked);
 };
 
-scrolling();
-*/
+
 /**
  * End Main Functions
  * Begin Events
  * 
 */
 
-// Build menu 
-
 // Scroll to Anchor. adding an event listener to check for active class 
 // upon active section of the viewport.
 window.addEventListener('scroll', sectionInvoked);
-
-
-// Scroll to section on link click
-
-let sectionOne = document.getElementsById("Section 1");
-let sectionTwo = document.getElementsById("Section 2");
-let sectionThree = document.getElementsById("Section 3");
-let sectionFour = document.getElementsById("Section 4");
-
-/**sectionOne.addEventListener('click', (e)=> {
-    document.documentElement.scrollTo(0, 200);
-};*/
-
-
-
-
-// Set sections as active
